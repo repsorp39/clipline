da33c982f68d2ac139f76b7d1067d0676cf7ef0e
Prosper AZA <158620336+repsorp39@users.noreply.github.com>
2024-12-01 01:25:57 +0100

    Update save.js

--- src/controllers/save.js
+++ src/controllers/save.js
@@ -3,7 +3,7 @@ const Clipboard = require("../models/Clipboard");
 const saveUpdate  = (socket) =>{ 
    return async (data )=>{
     try {
-        await Clipboard.updateOne({ name:data.origin } ,{ content:data.content });
+        await Clipboard.updateOne({ name:data.origin } ,{ content:data.content.replace(/\n/g,'<br>') });
         socket.broadcast.emit("saved" , {
             origin:data.origin,
             content:data.content
